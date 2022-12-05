293a2a8d0485008b9e9c67bcb7c09725d5d2aa54
Michael Corello <mchlcorello871@Gmail.com>
2022-12-05 08:19:04 -0500

    Crane 9001 really do be just 1 line

--- AdventofCode/Day5/Day5.cs
+++ AdventofCode/Day5/Day5.cs
@@ -15,16 +15,176 @@ namespace AdventofCode.Day5
         [TestCategory("Day5")]
         public void Part1()
         {
-            string[] lines = File.ReadAllLines(@"doooooooooooooooooog");
+            string[] input = File.ReadAllLines(@"C:\Users\Coral\source\repos\AdventofCode2022\AdventofCode\Day5\input");
+            var yard = File.ReadAllLines(@"C:\Users\Coral\source\repos\AdventofCode2022\AdventofCode\Day5\StorageYard.txt").ToList();
 
+            //string[] input = File.ReadAllLines(@"C:\Users\Coral\source\repos\AdventofCode2022\AdventofCode\Day5\input - Copy");
+            //var yard = File.ReadAllLines(@"C:\Users\Coral\source\repos\AdventofCode2022\AdventofCode\Day5\StorageYard - Copy.txt").ToList();
+
+            foreach (var item in yard)
+            {
+                item.Trim();
+            }
+
+            List<string> instructions = input.Skip(10).ToList();
+            //List<string> instructions = input.Skip(6).ToList();
+
+            List<List<string>> crates = new List<List<string>>() { new List<string>()};
+
+            var index = 0;
+            foreach (var crate in yard)
+            {
+                if(crate == string.Empty)
+                {
+                    crates.Add(new List<string>());
+                    index++;
+                }
+                else
+                {
+                    crates.ElementAt(index).Add(crate);
+                }
+            }
+
+
+
+            void ParseInstructions()
+            {
+
+                foreach (var line in instructions)
+                {
+                    var nums = new List<int>();
+                    var split = line.Split(' ');
+                    foreach (var item in split)
+                    {
+                        if (item.All(char.IsDigit))
+                        {
+                            nums.Add(Convert.ToInt32(item));
+                        }
+                    }
+
+                    //Take the range
+                    var move = nums[0];
+
+                    //Take the from index and create payload
+                    var from = nums[1];
+
+                    var payload = crates[from - 1].Take(move);
+
+                    //Reverse payload and place in new index
+                    var to = nums[2];
+
+                    foreach (var crate in payload)
+                    {
+                        var toIndex = crates[to - 1];
+
+                        var newOrder = toIndex.Prepend(crate).ToList();
+
+                        crates[to - 1] = newOrder;
+
+                    }
+
+                    crates[from - 1].RemoveRange(0, move);
+
+                }
+            }
+            ParseInstructions();
+
+            List<string> tops = new List<string>();
+
+            foreach (var crate in crates)
+            {
+                tops.Add(crate.First());
+                Console.Write(crate.First().ToString().Trim());
+            }
         }
 
         [TestMethod]
         [TestCategory("Day5")]
         public void Part2()
         {
-            string[] lines = File.ReadAllLines(@"doooooooooooooooooog");
+            string[] input = File.ReadAllLines(@"C:\Users\Coral\source\repos\AdventofCode2022\AdventofCode\Day5\input");
+            var yard = File.ReadAllLines(@"C:\Users\Coral\source\repos\AdventofCode2022\AdventofCode\Day5\StorageYard.txt").ToList();
 
+            //string[] input = File.ReadAllLines(@"C:\Users\Coral\source\repos\AdventofCode2022\AdventofCode\Day5\input - Copy");
+            //var yard = File.ReadAllLines(@"C:\Users\Coral\source\repos\AdventofCode2022\AdventofCode\Day5\StorageYard - Copy.txt").ToList();
+
+            foreach (var item in yard)
+            {
+                item.Trim();
+            }
+
+            List<string> instructions = input.Skip(10).ToList();
+            //List<string> instructions = input.Skip(6).ToList();
+
+            List<List<string>> crates = new List<List<string>>() { new List<string>() };
+
+            var index = 0;
+            foreach (var crate in yard)
+            {
+                if (crate == string.Empty)
+                {
+                    crates.Add(new List<string>());
+                    index++;
+                }
+                else
+                {
+                    crates.ElementAt(index).Add(crate);
+                }
+            }
+
+
+
+            void ParseInstructions()
+            {
+
+                foreach (var line in instructions)
+                {
+                    var nums = new List<int>();
+                    var split = line.Split(' ');
+                    foreach (var item in split)
+                    {
+                        if (item.All(char.IsDigit))
+                        {
+                            nums.Add(Convert.ToInt32(item));
+                        }
+                    }
+
+                    //Take the range
+                    var move = nums[0];
+
+                    //Take the from index and create payload
+                    var from = nums[1];
+
+                    var payload = crates[from - 1].Take(move);
+
+                    //Reverse payload and place in new index
+                    var to = nums[2];
+
+                    payload = payload.Reverse();
+
+                    foreach (var crate in payload)
+                    {
+                        var toIndex = crates[to - 1];
+
+                        var newOrder = toIndex.Prepend(crate).ToList();
+
+                        crates[to - 1] = newOrder;
+
+                    }
+
+                    crates[from - 1].RemoveRange(0, move);
+
+                }
+            }
+            ParseInstructions();
+
+            List<string> tops = new List<string>();
+
+            foreach (var crate in crates)
+            {
+                tops.Add(crate.First());
+                Console.Write(crate.First().ToString().Trim());
+            }
         }
 
 
